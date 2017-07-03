a12afe1c60c89221fb885fa273d080535c85db82
Dominic Torruellas <dtorruellas@gmail.com>
2017-07-03 13:00:53 -0700

    Add tests for DELETE /users/me/token

--- server/tests/users.test.js
+++ server/tests/users.test.js
@@ -125,4 +125,24 @@ describe('Testing user routes', () => {
         })
     })
   })
+
+  describe('DELETE /users/me/token', () => {
+    before(populateUsers)
+    it('should remove auth token on logout', (done) => {
+      request
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err) => {
+          if (err) {
+            return done(err)
+          }
+
+          User.findById(users[0]._id).then((user) => {
+            expect(user.tokens.length).toBe(0)
+            done()
+          }).catch(e => done(e))
+        })
+    })
+  })
 })
